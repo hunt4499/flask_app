0b0e7553ea35a5e6213b0defbf1c658e222bea95
ajaysood-fourbrick <68336583+ajaysood-fourbrick@users.noreply.github.com>
2021-03-10 16:07:15 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from flask import Flask
+
+basepath = Path()
+basedir = str(basepath.cwd())
+# Load the environment variables
+envars = basepath.cwd() / '.env'
+load_dotenv(envars)
+
 app = Flask(__name__)
+# app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
-    
